6aceaa3e221e796ab2270d980fb6ff41b863e2bf
Linfantee <145344308+Linfantee@users.noreply.github.com>
2023-10-24 14:24:06 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ej2.Raiz.Servicios;
+
+namespace Ej2.Raiz
+{
+    class Progam
+    {
+
+        static void Main(string[] args)
+        {
+            double raizCuadrada;
+            NumeroInterfaz aka = new NumeroImplementacion();
+            double ao = aka.peticionNumero();
+            raizCuadrada = Math.Sqrt(ao);
+
+            if (esEntero(raizCuadrada))
+            {
+                Console.WriteLine("la raíz cuadrada es entera, la raíz es:" + raizCuadrada);
+
+            }
+            else
+            {
+                Console.WriteLine("No es entera");
+            }
+
+            static bool esEntero(double aka)
+            {
+                return aka % 1 == 0;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Servicios/NumeroImplementacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2.Raiz.Servicios
+{
+    internal class NumeroImplementacion : NumeroInterfaz
+    {
+
+        public double peticionNumero()
+        {
+
+            double aka;
+            Console.WriteLine("Introduzca número");
+            aka = double.Parse(Console.ReadLine());     
+            return aka;
+        
+        }
+
+        
+        
+    }
+}
